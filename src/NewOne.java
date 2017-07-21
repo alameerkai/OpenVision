c29955b0837caf7a8529f45d375b42520a9d2624
Mohamed Yehia <alamersd1@gmail.com>
2017-07-21 15:45:16 +0200

    Message

---
+++ src/NewOne.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+/**
+ * Created by DELL on 21/07/2017.
+ */
+public class NewOne {
+
+    public static void main(String[] args) {
+/*
+
+        Set<String> un1 = new HashSet<>();
+        un1.add("Alameer");
+        un1.add("kaiser");
+        un1.add("aziz");
+        un1.add("Salman");
+
+        Set<String> un2 = new HashSet<>();
+        un2.add("Alameer");
+        un2.add("aziz");
+
+        System.out.println(un1.equals(un2));
+        System.out.println(un1.contains(un2));
+        System.out.println(un1.add("Ahmed"));
+        un1.clear();
+*/
+
+       /*
+        Scanner input = new Scanner(System.in);
+
+        List<Integer> un1 = new ArrayList<>();
+
+       un1.add(5);
+        un1.add(input.nextInt());
+        un1.clear();
+
+        System.out.println(un1);
+
+*/
+    }
+
+}
